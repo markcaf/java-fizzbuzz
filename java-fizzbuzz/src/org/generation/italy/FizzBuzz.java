f39228537301cc3dcf41496902337f9608500fb6
Marco Cafasso <marcocafassog@gmail.com>
2022-11-15 17:42:00 +0100

    feat: add FizzBuzz logic and print
    Is it a multiple of 3 & 5? FizzBuzz
    Is it a multiple of 3? Fizz
    Is it a multiple of 5? Buzz

--- java-fizzbuzz/src/org/generation/italy/FizzBuzz.java
+++ java-fizzbuzz/src/org/generation/italy/FizzBuzz.java
@@ -3,8 +3,17 @@ package org.generation.italy;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int i = 1; i <= 100; i++) 
+	    {
+	      if (((i % 3) == 0) && ((i % 5) == 0)) // Is it a multiple of 3 & 5?
+	        System.out.println("FizzBuzz");
+	      else if ((i % 3) == 0) // Is it a multiple of 3?
+	        System.out.println("Fizz");
+	      else if ((i % 5) == 0) // Is it a multiple of 5?
+	        System.out.println("Buzz");
+	      else
+	        System.out.println(i); // Not a multiple of 3 or 5
+	    }
 	}
 
 }
